b6b3ae03fa9e4653c5ed8b96beef27d5ed554cf3
david <david@martell.(none)>
2014-04-25 11:48:48 +0200

    Add config file

---
+++ config.js
@@ -0,0 +1,12 @@
+/* Configuration file for testing */
+
+exports.endpoint = 'ec2-54-199-108-216.ap-northeast-1.compute.amazonaws.com';
+exports.rushHost = 192.168.1.84;
+exports.rushUnsecPort = 5001;
+exports.rushSecPort = 5002;
+
+exports.imageArray = [
+  'img/img1.jpeg', 'img/img2.jpeg', 'img/img2.jpeg'
+];
+
+exports.captureInterval = 2000;
