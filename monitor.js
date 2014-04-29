8e7d3b4fe3ffe101d6ce6355da542cb9c0af1b64
david <david@martell.(none)>
2014-04-29 10:28:16 +0200

    Add memory monitor

---
+++ monitor.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+var pid = process.argv[2];
+var mem = 0;
+
+var getProcMem = function (pid) {
+  'use strict';
+  var data = fs.readFileSync('/proc/' + pid + '/status');
+
+  var elems = data.toString().split('\n');
+  elems = elems[15].split('\t');
+  elems = elems[1].split(' ');
+
+  return elems[elems.length - 2];
+};
+
+setInterval(function() {
+  'use strict';
+  mem = getProcMem(pid);
+  console.log('Memoria: ' + mem);
+  fs.appendFileSync('mem_data.csv', mem + '\n')
+}, 1000);
