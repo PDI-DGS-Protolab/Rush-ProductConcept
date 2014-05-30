acbe27322ec5302141bac7c24f6f5dad30487e53
dbr364 <dbarriga@conwet.com>
2014-05-30 10:26:05 +0200

    Update monitor

--- monitor.js
+++ monitor.js
@@ -1,37 +1,9 @@
 var fs = require('fs');
+var utils = require('./utils.js');
 
 var pid = process.argv[2];
 var mem, startTimeU, startTimeS,
       endTimeU, endTimeS, cpuUsage, cpuSys, cpuUser;
-
-var getUserUsage = function (pid) {
-  var data = fs.readFileSync('/proc/' + pid + '/stat');
-
-  var elems = data.toString().split(' ');
-  var utime = parseInt(elems[13]);
-  var stime = parseInt(elems[14]);
-
-  return utime + stime;
-}
-
-var getSysUsage = function () {
-  var data = fs.readFileSync('/proc/stat');
-
-  var elems = data.toString().split(' ');
-
-  return parseInt(elems[2]) + parseInt(elems[3]) + parseInt(elems[4]) + parseInt(elems[5]);
-};
-
-var getProcMem = function (pid) {
-  'use strict';
-  var data = fs.readFileSync('/proc/' + pid + '/status');
-
-  var elems = data.toString().split('\n');
-  elems = elems[15].split('\t');
-  elems = elems[1].split(' ');
-
-  return elems[elems.length - 2];
-};
 
 setInterval(function () {
   'use strict';
@@ -40,9 +12,9 @@ setInterval(function () {
   startTimeS = getSysUsage();
 
   setTimeout(function () {
-    mem = getProcMem(pid);
-    endTimeU = getUserUsage(pid);
-    endTimeS = getSysUsage();
+    mem = utils.getProcMem(pid);
+    endTimeU = utils.getUserUsage(pid);
+    endTimeS = utils.getSysUsage();
 
     cpuSys = endTimeS - startTimeS;
     cpuUser = endTimeU - startTimeU;
