f3d63d8e49118d4169bdda2ae4cd6c0155a83454
Fernando Godino <fgodino@conwet.com>
2014-06-09 11:07:20 +0200

    new server

--- simpleServer.js
+++ simpleServer.js
@@ -1,49 +1,12 @@
-var http = require('http');
-var config = require('./config');
+var express = require('express');
 var fs = require('fs');
+var app = express();
 
-var serverListener = function(connectedCallback, dataCallback, port) {
-  'use strict';
+app.post('/', function(req, res){
+    console.log('POST /');
+    res.end(200);
+});
 
-  var srv = http.createServer(function(req, res) {
-    var content = '', headers = req.headers, method = req.method, url = req.url;
-
-    var path = __dirname + '/' + Math.random();
-
-    var fd = fs.createWriteStream(path);
-
-    req.on('data', function(chunk) {
-      content += chunk;
-      fd.write(chunk);
-    });
-
-    req.on('end', function() {
-
-      fd.end();
-      fs.unlink(path);
-
-      res.writeHead(200, headers);
-      res.end('OK');
-      dataCallback(method, headers, url, content);
-    });
-
-    req.on('error', function(err){
-      console.log(err);
-    });
-
-    //srv.on('close', function () {
-    //    console.log('Server closed...');
-    //});
-  }).listen(port || config.targetServer.port, connectedCallback);
-
-  srv.on('error', function() {
-    dataCallback(null);
-  });
-
-  return srv;
-
-};
-
-serverListener(function() {
-  console.log('conectado');
-}, function() {}, 9000);
+port = 9000;
+app.listen(port);
+console.log('Listening at http://localhost:' + port)
